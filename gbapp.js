dde404417ea814e34d0cd77b56ac2964e7875556
NWMSU\S528130 <S528130@mail.nwmissouri.edu>
2016-10-13 10:23:11 -0500

    initial commit of full repository

--- gbapp.js
+++ gbapp.js
@@ -8,17 +8,43 @@ var server = require('http').createServer(app);
 
 
 
-app.get('/',function(req,res){
-    res.send('sup? bearcats!')
+
+// set up the view engine
+app.set("views", path.resolve(__dirname, "views")); // path to views
+app.set("view engine", "ejs"); // specify our view engine
+// manage our entries
+var entries = [];
+app.locals.entries = entries;
+// set up the logger
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+// GETS
+app.get("/", function (request, response) {
+  response.render("index");
+});
+app.get("/new-entry", function (request, response) {
+  response.render("new-entry");
 });
 
-app.set('view engine','ejs');
-// set up the view engine
-// manage our entries
-// set up the logger
-// GETS
 // POSTS
+app.post("/new-entry", function (request, response) {
+  if (!request.body.title || !request.body.body) {
+    response.status(400).send("Entries must have a title and a body.");
+    return;
+  }
+  entries.push({
+    title: request.body.title,
+    content: request.body.body,
+    published: new Date()
+  });
+  response.redirect("/");
+});
+
 // 404
+app.use(function (request, response) {
+  response.status(404).render("404");
+});
+
 
 // Listen for an application request on port 8081
 server.listen(8081, function () {
